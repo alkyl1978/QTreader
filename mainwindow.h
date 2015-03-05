f6e6bcaeb0c80bd7c44252dfa7392dabef5dfe95
alkyl1978 <alkyl1978@yandex.ru>
2015-03-05 14:48:36 +0300

    рабочая версия

--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 #include <QtSerialPort/QSerialPort>
 #include <QtSerialPort/QSerialPortInfo>
-#include <QtCore/QDebug>
+#include "port.h"
 
 namespace Ui {
 class MainWindow;
@@ -16,10 +16,26 @@ class MainWindow : public QMainWindow
 
 public:
     explicit MainWindow(QWidget *parent = 0);
+
     ~MainWindow();
+
+signals:
+
+    void savesettings(QString name, int baudrate, int DataBits, int Parity, int StopBits, int FlowControl);
+    void writeData(QByteArray data);
+
+private slots:
+    void on_Btn_Serch_clicked();
+    void checkCustomBaudRatePolicy(int idx);
+    void Print(QString data);
+    void on_cEnterText_returnPressed();
+    void on_BtnSave_clicked();
 
 private:
     Ui::MainWindow *ui;
+
+    Port PortNew;//
+
 };
 
 #endif // MAINWINDOW_H

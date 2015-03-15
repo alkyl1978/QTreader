97891383d18990f3855ac442b4f5085d0cddd9f5
alkyl1978 <alkyl1978@yandex.ru>
2015-03-15 19:01:23 +0300

    добавлено вывод всех доступных ком портов

--- form.cpp
+++ form.cpp
@@ -6,12 +6,20 @@ Form::Form(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Form)
 {
+    QList<QSerialPortInfo> com_ports = QSerialPortInfo::availablePorts();
+    QList<qint32> baud = QSerialPortInfo::standardBaudRates();
+    QSerialPortInfo port;
+
     ui->setupUi(this);
     ser=new serialPort(this);
     set=new QSettings("MyCompany", "MyApp");
     this->restoreGeometry(set->value("geometr").toByteArray());
-}
 
+    foreach(port, com_ports)
+        {
+            ui->cmbcom->addItem(port.portName());
+        }
+}
 Form::~Form()
 {
     delete ui;
